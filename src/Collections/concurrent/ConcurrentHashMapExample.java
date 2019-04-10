6257295323271fc56cdb998227e873ff019b0ea4
Gangula, Sudeep <sudeep.gangula@unisys.com>
2019-04-10 11:59:59 +0530

    changes

--- src/Collections/concurrent/ConcurrentHashMapExample.java
+++ src/Collections/concurrent/ConcurrentHashMapExample.java
@@ -3,16 +3,25 @@ package Collections.concurrent;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConcurrentHashMapExample {
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static void main(String[] args) {
 
-		ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
-		concurrentHashMap.put("A", "Apple");
-		concurrentHashMap.put("B", "Blackberry");
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public static void main(String[] args) {
 
-		concurrentHashMap.keySet().forEach(e -> {
-			System.out.println(concurrentHashMap.get(e));
-		});
+	ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+	concurrentHashMap.put("A", "Apple");
+	concurrentHashMap.put("B", "Blackberry");
 
-	}
+	concurrentHashMap.keySet().forEach(e -> {
+	    System.out.println(concurrentHashMap.get(e));
+	});
+
+	concurrentHashMap.forEach((k, v) -> {
+	    System.out.println(k + "   " + v);
+	});
+
+	concurrentHashMap.entrySet().forEach(item -> System.out.println(item));
+	
+	concurrentHashMap.values().forEach(value -> System.out.println(value));
+
+    }
 }
